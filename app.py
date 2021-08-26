aa2b879fecf1fc266c8f8395e3db6eb7ea76209d
jordan-hess <82159695+jordan-hess@users.noreply.github.com>
2021-08-26 13:05:48 +0200

    update

--- app.py
+++ app.py
@@ -190,25 +190,25 @@ def view_trend3():
         return jsonify(trend)
 
 
-# # this code allows you to see the 3rd trending shoe in the trending section
-# @app.route('/view-trends4/', methods=['GET'])
-# def view_trend4():
-#     trend = []
-#     try:
-#
-#         with sqlite3.connect('my_db.db') as connect:
-#             connect.row_factory = dict_factory
-#             cursor = connect.cursor()
-#             cursor.execute("SELECT * FROM trend where trend_id = 5")
-#             trend = cursor.fetchall()
-#
-#     except Exception as e:
-#         connect.rollback()
-#         print("There was an error fetching results from the database: " + str(e))
-#
-#     finally:
-#         connect.close()
-#         return jsonify(trend)
+# this code allows you to see the 4th trending shoe in the trending section
+@app.route('/view-trends4/', methods=['GET'])
+def view_trend4():
+    trend = []
+    try:
+
+        with sqlite3.connect('my_db.db') as connect:
+            connect.row_factory = dict_factory
+            cursor = connect.cursor()
+            cursor.execute("SELECT * FROM trend where trend_id = 6")
+            trend = cursor.fetchall()
+
+    except Exception as e:
+        connect.rollback()
+        print("There was an error fetching results from the database: " + str(e))
+
+    finally:
+        connect.close()
+        return jsonify(trend)
 
 
 # this code allows you to see the all trending shoes in the trending section
